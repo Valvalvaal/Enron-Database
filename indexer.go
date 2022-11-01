c1003e9245fd3d4d72c8b4c3089c8b03acc15938
Valeria Marin <valeriamarin193@gmail.com>
2022-11-01 18:55:42 -0500

    open data folder

--- indexer.go
+++ indexer.go
@@ -16,6 +16,10 @@ func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("Usage: ./indexer directory")
 	}
-	dat, err := os.ReadFile("")
-	fmt.Println(dat, err)
+	dirname := os.Args[1]
+	f, err := os.Open("./" + dirname)
+	fmt.Println(f.Name(), err)
+
+	files, err := f.Readdir(0)
+	fmt.Println(files, err)
 }
